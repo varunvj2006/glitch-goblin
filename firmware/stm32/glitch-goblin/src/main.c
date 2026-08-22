c688bae204b68b1512f055560746bfff5c0d6edc
varunvj2006 <mevarunvijay@gmail.com>
2026-08-22 19:15:39 -0400

    initial basic tests: blinking led for spio testing

--- firmware/stm32/glitch-goblin/src/main.c
+++ firmware/stm32/glitch-goblin/src/main.c
@@ -1,13 +1,38 @@
-#include <stdint.h>
+#include "stm32f4xx_hal.h"
+
+#define LED_PIN  GPIO_PIN_5
+#define LED_PORT GPIOA
+
+static void LED_Init(void);
 
 int main(void)
 {
-    volatile uint32_t counter = 0;
+    HAL_Init();
+
+    LED_Init();
 
     while (1)
     {
-        counter++;
+        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+        HAL_Delay(500);
     }
+}
 
-    return 0;
+static void LED_Init(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef gpio = {0};
+
+    gpio.Pin = LED_PIN;
+    gpio.Mode = GPIO_MODE_OUTPUT_PP;
+    gpio.Pull = GPIO_NOPULL;
+    gpio.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(LED_PORT, &gpio);
+}
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
 }
